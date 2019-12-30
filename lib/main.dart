700864958a3063cd5c5661278454678ec1acce70
Iwaide <iwaide@hoobby.net>
2019-12-31 02:12:17 +0900

    Basic Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(child: MyText('homogea')),
-  ));
+  runApp(
+    MaterialApp(
+      title: 'Myapp',
+      home: MyScaffold()
+    )
+  );
 }
 
-class MyText extends StatelessWidget {
-  MyText(this.text);
-  final String text;
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+  final Widget title;
+
   @override
   Widget build(BuildContext context) {
-    return Text(text);
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      height: 56.0,
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      child: Row(
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'NavigationMenu',
+            onPressed: null,
+          ),
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            onPressed: null,
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'ExampleTitle',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Hello World'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
